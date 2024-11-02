74e1bdef36b61277546a0a03798c0e69e3a0512b
Rafael <166154431+RafaelChoza@users.noreply.github.com>
2024-11-02 09:43:33 -0600

    first commit

--- src/pages/urls.py
+++ src/pages/urls.py
@@ -4,4 +4,6 @@ from pages import views
 
 urlpatterns = [
     path("", views.home, name="home"),
+    path("saludo/", views.saludo, name="saludo"),
+    path("productos/", views.productos, name="productos")
 ]
